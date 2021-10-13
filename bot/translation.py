5ffded6bb7f572e686a49bae36899454f15d2c14
botfilters <91194229+botfilters@users.noreply.github.com>
2021-10-13 23:31:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,7 +6,6 @@ class Translation(object):
     
     START_TEXT = """<b>Hey 🖐🏻 {} How are you?!!</b>
 <i>വെറുതെ start കുത്തി കളിക്കരുത്. സിനിമയുടെ ലിങ്ക് ആണ് വേണ്ടതെങ്കിൽ ഈ ഗ്രൂപ്പിൽ സിനിമയുടെ പേര് ടൈപ്പ് ചെയ്യ്, അപ്പോൽ ഞാൻ ലിങ്ക് തരാം എന്നിട്ട് ഇവിടെ വന്ന് start കൊടുക്കണം😜
-
 ഇനി ഇനേക്കൊണ്ട് വീണ്ടും വീണ്ടും പറയിപ്പിക്കരുത്. 🤷‍♂
 
 താഴെ കാണുന്ന ചാനലുകളിൽ ജോയിൻ ചെയ്യാൻ മറക്കരുതേ...🦧
@@ -21,42 +20,19 @@ class Translation(object):
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+-> കണ്ട ബട്ടൺ CLICK ചെയ്ത്  HELP ചോദിയ്ക്കാൻ ഇത് നിന്റെ വമ്പാടി തറവാട് അല്ല... കിട്ടുന്ന MOVIES DOWNLOAD ചെയ്തോണ്ട് പോയിക്കോണം...
+</i> 
             
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@LXRBOTSGROUP
+@FM_FilmMania
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> ©BOSS•⁰²✓</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/@malayalammoviesmmms">LXR-YOUTUBER</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/FM_FilmMania">FM_Admin_owner</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/LXR-YOUTUBER-AMAL/Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/botfilters/filmmania">Click Me</a></i>
 """
