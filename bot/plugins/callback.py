f0fe50357079dddde0b067f1bb11bdf131cec6f2
botfilters <91194229+botfilters@users.noreply.github.com>
2021-10-14 14:11:36 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,8 +142,8 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>⚜️ Check Links @FM_NEW_MOVIES Found ⚜️</i> <code>{leng}</code> <i>Results For Your request ✔️:</i> <code>{query}</code>"
-        
+    text=f"<i>⚜️ Check Links at @FM_NEW_MOVIES ⚜️Found</i> <code>{leng}</code> <i>Results For Your request ✔️:</i> <code>{query}</code>"
+      
     try:
         await update.message.edit(
                 text,
